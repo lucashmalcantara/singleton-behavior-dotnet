5bb0872fba11ce7b87a068c4274c593fba680c07
Lucas Alcântara <lucashmalcantara@gmail.com>
2022-03-23 08:34:02 -0300

    First version.

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # singleton-behavior-dotnet
-What is the behavior of the singleton pattern?
+
+What is the behavior of the singleton pattern? This project has some methods to understand how the singleton pattern works in .NET.
+
+
+
+# References
+
+[Padrões de Projeto: Singleton em C#](https://refactoring.guru/pt-br/design-patterns/singleton/csharp/example#example-2)
+
+[Implementing the Singleton Pattern in C#](https://csharpindepth.com/Articles/Singleton)
---
+++ SingletonBehavior/SingletonBehavior/Program.cs
@@ -0,0 +1,60 @@
+﻿using SingletonBehavior;
+
+Console.WriteLine("###### SINGLETON BEHAVIOUR ######");
+
+CreateInstanceBehavior();
+RunMethodBehavior();
+
+static void CreateInstanceBehavior()
+{
+    Console.WriteLine("\n\n# GET SINGLETON INSTANCE - MULTITHREADED ENVIRONMENT");
+
+    Console.WriteLine(
+        "\n{0}\n{1}\n\n{2}\n",
+        "If you see the same value, then singleton was reused (yay!)",
+        "If you see different values, then 2 singletons were created (booo!!)",
+        "RESULT:"
+        );
+
+    Thread process1 = new(() =>
+    {
+        GetSingletonInstance("FOO");
+    });
+    Thread process2 = new(() =>
+    {
+        GetSingletonInstance("BAR");
+    });
+
+    process1.Start();
+    process2.Start();
+
+    process1.Join();
+    process2.Join();
+}
+
+static void GetSingletonInstance(string value)
+{
+    Singleton singleton = Singleton.GetInstance(value);
+    Console.WriteLine(singleton.Value);
+}
+
+static void RunMethodBehavior()
+{
+    Console.WriteLine("\n\n# RUN METHOD - MULTITHREADED ENVIRONMENT");
+
+    Thread process1 = new(() =>
+    {
+        Singleton.GetInstance("FOO").LongRun(executionId: 1, millisecondsDelay: 5000);
+    });
+
+    Thread process2 = new(() =>
+    {
+        Singleton.GetInstance("BAR").LongRun(executionId: 2, millisecondsDelay: 5000);
+    });
+
+    process1.Start();
+    process2.Start();
+
+    process1.Join();
+    process2.Join();
+}
